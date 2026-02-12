660d980b7e334559f0a2ae76d548f96175c2419a
Karish289 <karishma.m28@hotmail.co.uk>
2026-02-12 23:33:52 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,19 @@
-document.addEventListener("DOMContentLoaded", () => {
 const beginBtn = document.getElementById("beginBtn");
-const messageBox = document.getElementById("messageBox");
 const yesBtn = document.getElementById("yesBtn");
+
+const intro = document.getElementById("intro");
+const messageBox = document.getElementById("messageBox");
 const final = document.getElementById("final");
+
 const song = document.getElementById("song");
 
-// When "Click to begin" is pressed
 beginBtn.addEventListener("click", () => {
-messageBox.classList.remove("hidden"); // show message
-beginBtn.style.display = "none";       // hide intro button
-
-```
-song.play().catch(() => {
-  console.log("Autoplay blocked until user interacts.");
-});
-```
-
+    intro.classList.add("hidden");
+    messageBox.classList.remove("hidden");
+    song.play();
 });
 
-// When "Yes" is pressed
 yesBtn.addEventListener("click", () => {
-final.classList.remove("hidden"); // show final photo
-messageBox.style.display = "none";
-});
+    messageBox.classList.add("hidden");
+    final.classList.remove("hidden");
 });
